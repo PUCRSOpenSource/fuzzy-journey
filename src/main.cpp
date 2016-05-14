7806691d5d89954c41cf85f17cb1e04b373a78a1
Matthias Nunes <matthiasnunes@gmail.com>
2016-05-14 03:09:27 -0300

    Closes #1
    Prints arruda's face

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include "datafile.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print_arruda()
+{
+	string line;
+	ifstream myfile("data/arruda.txt");
+	if (myfile)  // same as: if (myfile.good())
+	{
+		while (getline( myfile, line ))  // same as: while (getline( myfile, line ).good())
+		{
+			cout << line << endl;
+		}
+		myfile.close();
+	}
+	else
+		cout << "fooey\n";
+}
+
 
 int main()
 {
+	print_arruda();
 	return Datafile::init();
 }
