83ecfeca854f681898217a1ed68965dd1a625234
Matthias Nunes <matthiasnunes@gmail.com>
2016-06-25 19:15:50 -0300

    Implements insert in branch class, as in #3.

--- src/branch.cpp
+++ src/branch.cpp
@@ -3,7 +3,15 @@
 
 bool Branch::insert(uint32_t index, RowID ri)
 {
-	std::cout << "Branch: insert" << std::endl;
+	for (unsigned int i = 0; i < block.size(); i++)
+	{
+		if (block[i].getIndex() >= index)
+			return block[i].getLeft()->insert(index, ri);
+
+		if (i == block.size() - 1) // If last and not grater or equal
+			return block[i].getRight()->insert(index,ri);
+	}
+
 	return false;
 }
 
