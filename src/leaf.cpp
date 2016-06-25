77d1d75dabcb673eeacf80ee67095246428e2e64
Daniel Amarante <daniel.amarante2@gmail.com>
2016-06-25 19:07:38 -0300

    Add insert to Leaf.

--- src/leaf.cpp
+++ src/leaf.cpp
@@ -1,9 +1,19 @@
 #include "leaf.h"
 #include <iostream>
 
-bool Leaf::insert(uint32_t index, RowID ri)
+bool Leaf::insert(uint32_t index, RowID rowID)
 {
-	std::cout << "Leaf: insert" << std::endl;
+	for(size_t i = 0; i < block.size(); i++)
+	{
+		if (index == block[i].getIndex()) {
+			return false;
+		}
+		if (index > block[i].getIndex()) {
+			block.insert(block.begin() + i, LData(index, rowID));
+			return true;
+		}
+	}
+
 	return false;
 }
 
