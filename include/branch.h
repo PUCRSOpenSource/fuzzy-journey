034ca4a4a248085fb929138a305f4c3426747839
Matthias Nunes <matthiasnunes@gmail.com>
2016-06-25 18:15:38 -0300

    Completely creates branch and node structers mentioned in #3

--- include/branch.h
+++ include/branch.h
@@ -3,13 +3,18 @@
 
 #include "node.h"
 #include "rowid.h"
+#include "bdata.h"
 #include <cstdint>
+#include <vector>
 
 class Branch : public Node
 {
 	public:
 		virtual bool insert(uint32_t index, RowID ri);
 		virtual RowID select(uint32_t index);
+
+	private:
+		std::vector<BData> block;
 };
 
 #endif /* BRANCH_H */
--- include/leaf.h
+++ include/leaf.h
@@ -3,13 +3,18 @@
 
 #include "node.h"
 #include "rowid.h"
+#include "ldata.h"
 #include <cstdint>
+#include <vector>
 
 class Leaf : public Node
 {
 	public:
 		virtual bool insert(uint32_t index, RowID ri);
 		virtual RowID select(uint32_t index);
+
+	private:
+		std::vector<LData> block;
 };
 
 #endif /* LEAF_H */
